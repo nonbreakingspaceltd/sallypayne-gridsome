b89b455f50eeb7b4a6ef9c4a2a50db6e48a263d2
Paul Welsh <spacedawwwg@gmail.com>
2020-02-04 18:26:10 +0000

    feat: add Wordpress config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,28 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+require('dotenv').config();
+
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
-}
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: process.env.WORDPRESS_URL, // required
+        apiBase: 'wp-json',
+        typeName: 'WordPress',
+        perPage: 100,
+        concurrent: 10
+      }
+    }
+  ],
+  templates: {
+    WordPressPost: '/journal/:slug',
+    // WordPressProduct: '/shop/:slug',
+    // WordPressWork: '/work/:slug'
+  }
+};
---
+++ src/templates/WordPressPost.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <h1 v-html="$page.wordPressPost.title" />
+    <div v-html="$page.wordPressPost.content" />
+  </Layout>
+</template>
+
+<page-query>
+query WordPressPost ($id: ID!) {
+  wordPressPost(id: $id) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+  export default {
+    metaInfo() {
+      return {
+        title: this.$page.wordPressPost.title
+      };
+    }
+  };
+</script>
