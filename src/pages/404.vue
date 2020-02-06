714f468d1be965c1563ab1edb81030e306f4739d
Paul Welsh <spacedawwwg@gmail.com>
2020-02-06 11:16:19 +0000

    feat: add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <div class="sp-l-page">
+      <div class="sp-l-page__inner">
+        <h1 class="sp-o-pagetitle">
+          Oh dear. Sorry, but this page does not exist!
+        </h1>
+      </div>
+    </div></Layout
+  >
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: `404 - Page not found`
+    }
+  };
+</script>
