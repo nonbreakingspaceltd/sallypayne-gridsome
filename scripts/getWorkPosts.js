29be49d90d70377f015751a5216a45e2c2c9844b
Paul Welsh <spacedawwwg@gmail.com>
2020-02-16 13:05:08 +0000

    feat: add filename fallback

--- scripts/getWorkPosts.js
+++ scripts/getWorkPosts.js
@@ -1,14 +1,14 @@
-const playwright = require("playwright");
-const fs = require("fs");
+const playwright = require('playwright');
+const fs = require('fs');
 
 async function getItemsForType(page, type) {
   await page.goto(`https://sallypayne.com/${type}/`);
-  const workItems = await page.$$(".et_pb_gallery_image");
+  const workItems = await page.$$('.et_pb_gallery_image');
   console.log(`${workItems.length} items found for ${type}`);
   const items = [];
   for (const workItem of workItems) {
-    const title = await workItem.$eval("a", node => node.title);
-    const href = await workItem.$eval("a", node => node.href);
+    const title = await workItem.$eval('a', node => node.title || node.href.split('/').pop().split('.')[0]);
+    const href = await workItem.$eval('a', node => node.href);
     items.push({ title, href });
   }
   return items;
@@ -16,8 +16,8 @@ async function getItemsForType(page, type) {
 
 (async () => {
   try {
-    const types = ["illustration", "surface-pattern-design", "work-projects"];
-    const browser = await playwright["chromium"].launch();
+    const types = ['illustration', 'surface-pattern-design', 'work-projects'];
+    const browser = await playwright['chromium'].launch();
     const context = await browser.newContext();
     const results = [];
     for (const type of types) {
@@ -25,7 +25,7 @@ async function getItemsForType(page, type) {
       const items = await getItemsForType(page, type);
       results.push({ type, items });
     }
-    fs.writeFileSync("data/types.json", JSON.stringify(results, null, 2));
+    fs.writeFileSync('data/types.json', JSON.stringify(results, null, 2));
     await browser.close();
   } catch (err) {
     console.log(err);
